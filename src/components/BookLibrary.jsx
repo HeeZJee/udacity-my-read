f030157eba2ad3b3ec898055a6145f1fa7133797
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2020-11-05 00:20:48 +0500

    My Book Read v0.1

--- src/components/BookLibrary.jsx
+++ src/components/BookLibrary.jsx
@@ -29,7 +29,7 @@ class BookLibrary extends Component {
         return (
             <div className="list-books">
                 <div className="list-books-title">
-                    <h1>MyReads</h1>
+                    <h1>My Books Library</h1>
                 </div>
                 <div className="list-books-content">
                     <div>
